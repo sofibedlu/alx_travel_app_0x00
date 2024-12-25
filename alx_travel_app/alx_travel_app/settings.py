15a8cb55955a1f0e6e2898ae0cec6a24f0a66b5f
sofy <bedlusofi@gmail.com>
2024-12-25 10:59:13 +0300

    update the directory structure

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -19,12 +19,11 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Reading .env file
-environ.Env.read_env(env_file=str(BASE_DIR / '.env'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Reading .env file
+environ.Env.read_env(env_file=str(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
